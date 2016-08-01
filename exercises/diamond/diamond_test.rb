ae111964d00a4b4318635073b88937702fc340d0
leonardo <l.castillo1989@Hotmail.com>
2016-08-01 15:27:06 -0400

    #350 Added solution and test for Diamond with correct indentions

---
+++ exercises/diamond/diamond_test.rb
@@ -0,0 +1,31 @@
+#!/usr/bin/env ruby
+gem 'minitest', '>= 5.0.0'
+require 'minitest/autorun'
+require_relative 'diamond'
+
+class DiamondTest < Minitest::Test
+  def test_letter_A
+    answer = Diamond.make_diamond('A')
+    assert_equal "A\n", answer
+  end
+
+  def test_letter_C
+    skip
+    answer = Diamond.make_diamond("C")
+    string = "  A  \n B B \nC   C\n B B \n  A  \n"
+    assert_equal string, answer
+  end
+
+  def test_letter_E
+    skip
+    answer = Diamond.make_diamond("E")
+    string = "    A    \n   B B   \n  C   C  \n D     D \nE       "+
+             "E\n D     D \n  C   C  \n   B B   \n    A    \n"
+    assert_equal string, answer
+  end
+
+  def test_bookkeeping
+    skip
+    assert_equal 1, Dime::VERSION
+  end
+end
